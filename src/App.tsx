9d7de3701c8b2350554054819e6ec634793c41c5
Francisco <ip4c0@icloud.com>
2022-06-22 22:35:32 -0500

    Initial commit | Crazy Code

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function addToCounterHandler (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) 
+  {
+    setCount( count + 1 )
+  }
+
+  const substractCounterHanlder = ( event:React.MouseEvent<HTMLButtonElement, MouseEvent> ) => setCount(count -1 )
+
+  return (
+    <div className="App">
+    <h1>Counter</h1>
+    <h2>{count}</h2>
+    <div>
+    <button onClick={addToCounterHandler}> ADD</button>
+
+    </div>
+    <div>
+    <button onClick={ substractCounterHanlder }>SUBSTRACT </button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
